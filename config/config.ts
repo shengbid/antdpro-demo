38bd74f4472bd17bb7feda91409f8536c7148687
laism <laism@chinacsci.com>
2021-09-14 14:24:48 +0800

    feat: 修改上传组件

--- config/config.ts
+++ config/config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
     // default true, when it is true, will use `navigator.language` overwrite default
     baseNavigator: true,
   },
+  devServer: {
+    port: 8090
+  },
   dynamicImport: {
     loading: '@ant-design/pro-layout/es/PageLoading',
   },
