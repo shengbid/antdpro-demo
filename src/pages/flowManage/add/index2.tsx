6faaee51778e04220ae6fab00a59ff11011644e1
laism <laism@chinacsci.com>
2022-04-11 09:10:17 +0800

    feat: 流程测试数据

---
+++ src/pages/flowManage/add/index2.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, Background } from 'react-flow-renderer';
+
+// import ConnectionLine from './ConnectionLine';
+
+const initialNodes = [
+  {
+    id: 'connectionline-1',
+    type: 'input',
+    data: { label: 'Node 1' },
+    position: { x: 250, y: 5 },
+  },
+];
+
+const ConnectionLineFlow = () => {
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      // connectionLineComponent={ConnectionLine}
+      onConnect={onConnect}
+    >
+      <Background />
+    </ReactFlow>
+  );
+};
+
+export default ConnectionLineFlow;
