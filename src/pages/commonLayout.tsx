1558d1f789516c716c91a29ad175b41058534d66
shengbid <441974767@qq.com>
2022-05-09 18:09:09 +0800

    fix: 菜单缓存数组处理

--- src/pages/commonLayout.tsx
+++ src/pages/commonLayout.tsx
@@ -2,41 +2,46 @@ import React, { useEffect, useState } from 'react';
 import { PageContainer } from '@ant-design/pro-layout';
 import { history, useModel } from 'umi';
 import { find } from 'lodash';
-
-export type menuItemProps = {
-  tab: string;
-  key: string;
-  pathname: string;
-};
+import type { menuTabProps } from '@/services/types';
+import { handleMenusToFlat } from '@/utils/base';
 
 const CommonLayout: React.FC = (props: any) => {
-  const { location } = props;
+  const { location, route } = props;
+  const { pathname } = location;
   const [activeKey, setActiveKey] = useState<string>('');
   const { exitMenus, updateMenus } = useModel('exitMenus', (model) => ({
     exitMenus: model.exitMenus,
     updateMenus: model.updateMenus,
   }));
 
+  const flatMenus = handleMenusToFlat(route.routes);
+
   useEffect(() => {
-    console.log(333, exitMenus);
-    const arr: menuItemProps[] = exitMenus.filter(
-      (item: menuItemProps) => item.pathname !== location.pathname,
-    );
+    // console.log(333, location, flatMenus);
+    const arr: menuTabProps[] = exitMenus.filter((item: menuTabProps) => item.key !== pathname);
     if (arr.length === exitMenus.length) {
-      setActiveKey(location.key);
-      arr.push({
-        tab: location.key,
-        key: location.key,
-        pathname: location.pathname,
-      });
+      const activeTab = find(flatMenus, (item) => item.pathname === pathname) || {};
+      const activeMenu: menuTabProps = {
+        tab: activeTab.name,
+        key: pathname,
+        closable: exitMenus.length > 0, // 新增时,第一个页面不能删除
+      };
+      arr.push(activeMenu);
+
       updateMenus(arr);
+    } else if (exitMenus.length === 1) {
+      // 删除时,只剩一个标签去掉删除图标
+      const data = exitMenus;
+      data[0].closable = false;
+      updateMenus(data);
     }
+    setActiveKey(pathname);
   }, [location]);
 
-  const onTabChange = (key) => {
-    const activeTab = find(exitMenus, (item) => item.key === key) || {};
-    console.log(key, activeTab);
-    history.push(activeTab.pathname);
+  const onTabChange = (key: string) => {
+    history.push(key);
+    setActiveKey(key);
+    console.log(key);
   };
   return (
     <PageContainer
@@ -52,11 +57,20 @@ const CommonLayout: React.FC = (props: any) => {
         tabBarStyle: {
           paddingBottom: '3px',
         },
-        onEdit: (e, action) => {
-          console.log(e, action);
-          const arr: menuItemProps[] = exitMenus.filter(
-            (item: menuItemProps) => item.pathname !== location.pathname,
-          );
+        onEdit: (path) => {
+          // console.log(path, action, pathname);
+          let activePath = pathname;
+          const arr: menuTabProps[] = exitMenus.filter((item: menuTabProps, i: number) => {
+            if (item.key === pathname) {
+              // 获取前一个标签
+              activePath = exitMenus[i - 1].key;
+            }
+            return item.key !== path;
+          });
+          // 如果关闭当前标签,展示前一个标签
+          if (path === pathname) {
+            history.push(activePath);
+          }
           updateMenus(arr);
         },
       }}
