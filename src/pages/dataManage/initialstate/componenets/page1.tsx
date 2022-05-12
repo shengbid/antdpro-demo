05e1426910d9ab470091dc843cf09a147f40a156
shengbid <441974767@qq.com>
2022-05-12 11:37:12 +0800

    fix: 去掉无效引用

--- src/pages/dataManage/initialstate/componenets/page1.tsx
+++ src/pages/dataManage/initialstate/componenets/page1.tsx
@@ -3,95 +3,94 @@ import { useModel } from 'umi';
 import { Descriptions, Form, Input, Button, Row, Col } from 'antd';
 
 export default () => {
-  const { initialState, loading, error, refresh, setInitialState } = useModel('@@initialState');
-  
-  console.log(initialState)
-  const { currentUser } = initialState
+  const { initialState, setInitialState } = useModel('@@initialState');
+
+  const { currentUser } = initialState;
 
   const onFinish = (values: any) => {
     console.log('Success:', values);
-    setInitialState({...initialState, currentUser: values})
-  }
+    setInitialState({ ...initialState, currentUser: values });
+  };
 
   const onFinishFailed = (errorInfo: any) => {
     console.log('Failed:', errorInfo);
-  }
+  };
 
-  return <>
-    <h2>页面一</h2>
-    <Descriptions title="登陆信息">
-      <Descriptions.Item label="UserName">{currentUser.name}</Descriptions.Item>
-      <Descriptions.Item label="Telephone">{currentUser.phone}</Descriptions.Item>
-      <Descriptions.Item label="group">{currentUser.group}</Descriptions.Item>
-      <Descriptions.Item label="signature">{currentUser.signature}</Descriptions.Item>
-      <Descriptions.Item label="Address">
-      {currentUser.address}
-      </Descriptions.Item>
-    </Descriptions>
-    <h2>修改初始数据</h2>
-    <Form
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-      initialValues={currentUser}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-    >
-      <Row gutter={24}>
-      <Col span={8}>
-        <Form.Item
-          label="Username"
-          name="name"
-          rules={[{ required: true, message: 'Please input your username!' }]}
-        >
-          <Input />
+  return (
+    <>
+      <h2>页面一</h2>
+      <Descriptions title="登陆信息">
+        <Descriptions.Item label="UserName">{currentUser.name}</Descriptions.Item>
+        <Descriptions.Item label="Telephone">{currentUser.phone}</Descriptions.Item>
+        <Descriptions.Item label="group">{currentUser.group}</Descriptions.Item>
+        <Descriptions.Item label="signature">{currentUser.signature}</Descriptions.Item>
+        <Descriptions.Item label="Address">{currentUser.address}</Descriptions.Item>
+      </Descriptions>
+      <h2>修改初始数据</h2>
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={currentUser}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Row gutter={24}>
+          <Col span={8}>
+            <Form.Item
+              label="Username"
+              name="name"
+              rules={[{ required: true, message: 'Please input your username!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={8}>
+            <Form.Item
+              label="Telephone"
+              name="phone"
+              rules={[{ required: true, message: 'Please input your Telephone!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={8}>
+            <Form.Item
+              label="group"
+              name="group"
+              rules={[{ required: true, message: 'Please input your group!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={8}>
+            <Form.Item
+              label="signature"
+              name="signature"
+              rules={[{ required: true, message: 'Please input your signature!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+
+          <Col span={8}>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true, message: 'Please input your address!' }]}
+            >
+              <Input />
+            </Form.Item>
+          </Col>
+        </Row>
+        <Form.Item wrapperCol={{ offset: 3, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            修改
+          </Button>
         </Form.Item>
-      </Col>
-
-      <Col span={8}>
-      <Form.Item
-        label="Telephone"
-        name="phone"
-        rules={[{ required: true, message: 'Please input your Telephone!' }]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-
-      <Col span={8}>
-      <Form.Item
-        label="group"
-        name="group"
-        rules={[{ required: true, message: 'Please input your group!' }]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-
-      <Col span={8}>
-      <Form.Item
-        label="signature"
-        name="signature"
-        rules={[{ required: true, message: 'Please input your signature!' }]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-
-      <Col span={8}>
-      <Form.Item
-        label="Address"
-        name="address"
-        rules={[{ required: true, message: 'Please input your address!' }]}
-      >
-        <Input />
-      </Form.Item>
-      </Col>
-      </Row>
-      <Form.Item wrapperCol={{ offset: 3, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          修改
-        </Button>
-      </Form.Item>
-    </Form>
-  </>
+      </Form>
+    </>
+  );
 };
--- src/pages/dataManage/useModel/index.tsx
+++ src/pages/dataManage/useModel/index.tsx
@@ -1,20 +1,21 @@
 // import React from 'react'
-import Page1 from './componenets/page1'
-import Page2 from './componenets/page2'
-import Page3 from './componenets/page3'
+import Page1 from './componenets/page1';
+import Page2 from './componenets/page2';
+import Page3 from './componenets/page3';
 
 export default () => {
-  
-  return <>
-    <h2>useModel数据管理</h2>
-    <div style={{backgroundColor: '#fff', marginTop: '20px', padding: '10px'}}>
-      <Page1 />
-    </div>
-    <div style={{backgroundColor: '#fff', marginTop: '20px', padding: '10px'}}>
-      <Page2 />
-    </div>
-    <div style={{backgroundColor: '#fff', marginTop: '20px', padding: '10px'}}>
-      <Page3 />
-    </div>
-  </>
+  return (
+    <>
+      <h2>useModel数据管理</h2>
+      <div style={{ backgroundColor: '#fff', marginTop: '20px', padding: '10px' }}>
+        <Page1 />
+      </div>
+      <div style={{ backgroundColor: '#fff', marginTop: '20px', padding: '10px' }}>
+        <Page2 />
+      </div>
+      <div style={{ backgroundColor: '#fff', marginTop: '20px', padding: '10px' }}>
+        <Page3 />
+      </div>
+    </>
+  );
 };
