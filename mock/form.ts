fc1e41ec3da0e6490086d2be3e3ff9d90bf57e6c
shengbid <441974767@qq.com>
2022-05-13 17:32:55 +0800

    fix: 上传数据测试

--- mock/form.ts
+++ mock/form.ts
@@ -1,18 +1,19 @@
 import { Request, Response } from 'express';
-import Mock from 'mockjs'
-const Random = Mock.Random
+import Mock from 'mockjs';
+const Random = Mock.Random;
 
 const uploadData = (req: Request, res: Response) => {
-  const color = Random.color()
-  const text = Random.word(4, 10)
+  const color = Random.color();
+  const text = Random.word(4, 10);
 
   res.send({
     data: {
-      fileUrl: Random.image('400x300', color, '#FFF', text)
+      fileUrl: Random.image('400x300', color, '#FFF', text),
+      // fileUrl: '8888'
     },
-    success: true
-  })
-}
+    success: true,
+  });
+};
 
 export default {
   'POST /api/upload/file': uploadData,
